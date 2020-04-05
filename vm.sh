c00cee2b1d2e6de9a602ba83d250b846b8344966
admin <59332227+basetrue@users.noreply.github.com>
2020-04-05 21:49:53 +0700

    Update vm.sh

--- vm.sh
+++ vm.sh
@@ -1,2 +1,2 @@
 #!/bin/bash 
-az vm extension set --name NvidiaGpuDriverWindows --publisher Microsoft.HpcCompute.NvidiaGpuDriverWindows --version 1.3.1.0 --ids /subscriptions/9482f577-85eb-43ec-83bf-e981be657fa2/resourceGroups/server-group/providers/Microsoft.Compute/virtualMachines/windows7/extensions/NvidiaGpuDriverWindows
+az vm extension set --name NvidiaGpuDriverWindows --publisher Microsoft.HpcCompute --version 1.3.1.0 --ids /subscriptions/9482f577-85eb-43ec-83bf-e981be657fa2/resourceGroups/server-group/providers/Microsoft.Compute/virtualMachines/windows7/extensions/NvidiaGpuDriverWindows
