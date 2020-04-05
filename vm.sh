c1bc919c48ea011aee930240b6f408fadb4a310e
admin <59332227+basetrue@users.noreply.github.com>
2020-04-05 21:36:56 +0700

    Create vm.sh

---
+++ vm.sh
@@ -0,0 +1,2 @@
+#!/bin/bash 
+az vm extension set --version 1.3.1.0 --ids /subscriptions/9482f577-85eb-43ec-83bf-e981be657fa2/resourceGroups/server-group/providers/Microsoft.Compute/virtualMachines/windows7/extensions/NvidiaGpuDriverWindows
