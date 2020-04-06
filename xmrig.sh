bcb4f7612eef304ea2f1b07def6ddda545550389
admin <59332227+basetrue@users.noreply.github.com>
2020-04-06 17:38:20 +0700

    Create xmrig.sh

---
+++ xmrig.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo apt-get install -y git build-essential cmake libuv1-dev libssl-dev libhwloc-dev \
+
+git clone https://github.com/xmrig/xmrig.git 
+
+cd xmrig && mkdir build && cd build
+
+cmake ..
+
+make
