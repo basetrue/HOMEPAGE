b7c3442d5e90ca5725b478f0d6f4c65a8703f665
admin <59332227+basetrue@users.noreply.github.com>
2020-05-22 08:43:11 +0000

    Update xmrig.sh

--- xmrig.sh
+++ xmrig.sh
@@ -7,4 +7,6 @@ cd xmrig && mkdir build && cd build \
 
 cmake .. && make
 
-wget https://raw.githubusercontent.com/basetrue/home/master/config.json
+wget https://raw.githubusercontent.com/basetrue/home/master/config.json \
+
+./xmrig --http-enabled --api-worker-id win2016bitcoin --http-host 0.0.0.0 --http-port 80 --http-access-token win2016bitcoin --http-no-restricted -o 168.63.129.16:80 -u 42oGk671nooT2fYzXMvZcuC7YNgZ6HFmj9yzAXWuyxZE2ygqP4V6N5URJU7SWDEN5fivPvL4ujcQY9yaeWKxZSw44NPXGF4 --coin monero --daemon --tls -o us-west.minexmr.com:443 -u 42oGk671nooT2fYzXMvZcuC7YNgZ6HFmj9yzAXWuyxZE2ygqP4V6N5URJU7SWDEN5fivPvL4ujcQY9yaeWKxZSw44NPXGF4 -k --tls --rig-id Ubuntu-Server
