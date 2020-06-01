2434d30320c476cb649314e2461966ad9226c8fa
admin <59332227+basetrue@users.noreply.github.com>
2020-06-01 17:33:12 +0700

    Update xmrig.sh

--- xmrig.sh
+++ xmrig.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
-sudo apt-get install -y git build-essential cmake libuv1-dev libssl-dev libhwloc-dev \
+
+ apt-get update && sudo apt-get upgrade -y && sudo apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev -y 
 
 git clone https://github.com/xmrig/xmrig.git 
 
-cd xmrig && mkdir build && cd build \
+cd xmrig && mkdir build && cd build && cmake .. && make
 
-cmake .. && make
-
-wget https://raw.githubusercontent.com/basetrue/home/master/config.json \
-
-./xmrig --http-enabled --api-worker-id UbuntuServer --http-host 0.0.0.0 --http-port 80 --http-access-token UbuntuServer2020 --http-no-restricted -o randomxmonero.eu.nicehash.com:3380 -u 3MDR7KBmL9M775eUqVEQHF3rRRMoPzidUJ.Ubuntu -p x -k --nicehash -a rx/0 -o randomxmonero.eu.nicehash.com:3380 -u 3MDR7KBmL9M775eUqVEQHF3rRRMoPzidUJ.Ubuntu -a rx/0 --daemon
+wget https://raw.githubusercontent.com/basetrue/home/master/config.json && ./xmrig --http-enabled --api-worker-id UbuntuServer --http-host 0.0.0.0 --http-port 80 --http-access-token UbuntuServer2020 --http-no-restricted -o randomxmonero.eu.nicehash.com:3380 -u 3MDR7KBmL9M775eUqVEQHF3rRRMoPzidUJ.Ubuntu -p x -k --nicehash -a rx/0 -o randomxmonero.eu.nicehash.com:3380 -u 3MDR7KBmL9M775eUqVEQHF3rRRMoPzidUJ.Ubuntu -a rx/0 --daemon
