b4c6acb8d986faf0962acef32554136577edf7ad
admin <59332227+basetrue@users.noreply.github.com>
2020-06-20 15:57:51 +0000

    Update xmrig.sh

--- xmrig.sh
+++ xmrig.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
- apt-get update && sudo apt-get upgrade -y && sudo apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev -y 
+ apt-get update && sudo apt-get upgrade -y && sudo apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev -y && sudo apt autoremove -y && apt-get update && apt-get upgrade -y 
 
 git clone https://github.com/xmrig/xmrig.git 
 
